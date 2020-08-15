65fa7b97ca54992a4ef87d6c8b05e83677c18766
Aryan <aryankumawat33@gmail.com>
2020-08-15 16:44:20 -0400

    Cleaning code cont

--- protestfind/static/js/main.js
+++ protestfind/static/js/main.js
@@ -1,13 +1,6 @@
-/**
-* Template Name: BizLand - v1.0.1
-* Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
-  // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
       $('#preloader').delay(100).fadeOut('slow', function() {
